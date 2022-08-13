9621688bb6fdaf9d03d10730e9bdd974efa7b5b3
kcTest <763336180@qq.com>
2022-08-13 13:58:09 +0800

    netty server client demo

--- Netty/src/main/java/com/zkc/netty/ClientDemo01.java
+++ Netty/src/main/java/com/zkc/netty/ClientDemo01.java
@@ -13,23 +13,27 @@ import java.util.Scanner;
 public class ClientDemo01 {
 	
 	public static void main(String[] args) throws InterruptedException {
-		//1、创建启动器
+		/* 1、创建启动器 */
 		Channel channel = new Bootstrap()
-				//2、添加EventLoop
+				/* 2、添加EventLoop */
 				.group(new NioEventLoopGroup())
-				//3、选择客户端的channel实现  NioSocketChannel：基于NIO的SocketChannel的实现
+				/* 3、选择客户端的channel实现  NioSocketChannel：基于NIO的SocketChannel的实现 */
 				.channel(NioSocketChannel.class)
-				//4、添加事件处理器  ；连接建立后开始初始化
+				/* 4、添加事件处理器  ；与服务器建立连接后开始初始化 */
 				.handler(new ChannelInitializer<NioSocketChannel>() {
 					@Override
 					protected void initChannel(NioSocketChannel ch) throws Exception {
+						//StringEncoder: string转成ByteBuf
 						ch.pipeline().addLast(new StringEncoder());
 					}
-					//5、连接服务器
-				}).connect(new InetSocketAddress(8080))
+				})
+				/* 5、连接服务器 */
+				.connect(new InetSocketAddress(8080))
+				//阻塞 直到连接建立
 				.sync()
+				//获取建立连接后的通道
 				.channel();
-		//向服务器发送数据
+		/* 6、向服务器发送数据 */
 		Scanner scanner = new Scanner(System.in);
 		while (scanner.hasNext()) {
 			String msg = scanner.nextLine();
--- Netty/src/main/java/com/zkc/netty/ServerDemo01.java
+++ Netty/src/main/java/com/zkc/netty/ServerDemo01.java
@@ -18,30 +18,34 @@ public class ServerDemo01 {
 	public static void main(String[] args) {
 		/* 1、启动器：负责组装Netty组件 启动服务器 */
 		new ServerBootstrap()
-				/* 2、设置EventLoopGroup  包含一个或者多个 EventLoop   由selector+thread组成
-				  一个 EventLoop可能会被分配给一个或多个Channel 处理已注册的channel的相关事件 */
+				/* 2、 创建并分配一个 NioEventLoopGroup 实例以进行事件的处理，如接受新连接以及读/写数据
+				       EventLoopGroup包含一个或者多个EventLoop   
+				       一个EventLoop本质是一个单线程执行器（NioEventLoop:selector+thread) 可能服务多个Channel */
 				.group(new NioEventLoopGroup())
-				/* 3、选择使用的通道 支持NIO BIO。 NioServerSocketChannel：基于NIO的ServerSocketChannel的实现 */
+				/* 3、选择服务端使用的通道实现 支持NIO BIO。 NioServerSocketChannel：基于NIO的ServerSocketChannel的实现 */
 				.channel(NioServerSocketChannel.class)
-				/* 4、指定worker要做的事   5、初始化 负责对已连接客户端进行数据读写的 NioSocketChannel（sc）；添加handler */
-				.childHandler(new ChannelInitializer<NioSocketChannel>() {
-					@Override
-					protected void initChannel(NioSocketChannel ch) throws Exception {
-						/* 6、添加具体的事件处理器 */
-						ch.pipeline()
-								//StringDecoder: 将Netty的ByteBuf转为String
-								.addLast(new StringDecoder())
-								// ChannelInboundHandlerAdapter：处理入站数据以及各种状态变化
-								.addLast(new ChannelInboundHandlerAdapter() {
-									//传入消息时被调用
-									@Override
-									public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-										//消息已被StringDecoder转换
-										LOGGER.info(msg.toString());
-									}
-								});
-					}
-				})
+				/* 4、childHandler()：SocketChannel使用，设置与客户端通道相关的事件 
+				handler()：ServerSocketChannel使用  设置与服务端通道相关的事件*/
+				.childHandler(
+						/*5、连接建立后  开始初始化 负责对已连接客户端进行数据读写的NioSocketChannel（sc）*/
+						new ChannelInitializer<NioSocketChannel>() {
+							@Override
+							protected void initChannel(NioSocketChannel ch) throws Exception {
+								/* 6、添加具体的事件处理器 */
+								ch.pipeline()
+										//StringDecoder: 将Netty的ByteBuf转为String
+										.addLast(new StringDecoder())
+										// ChannelInboundHandlerAdapter：处理入站数据以及各种状态变化
+										.addLast(new ChannelInboundHandlerAdapter() {
+											//传入消息时被调用
+											@Override
+											public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+												//消息已被StringDecoder转换
+												LOGGER.info(msg.toString());
+											}
+										});
+							}
+						})
 				/* 7、绑定监听端口 */
 				.bind(8080);
 	}
